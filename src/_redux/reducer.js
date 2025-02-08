5bfedec499220f8acff9c2db9132bc42a28aeb32
Asim-Zaheer <iasimzaheer@gmail.com>
2025-02-08 23:42:27 +0500

    redux remove name change

--- src/_redux/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import counterSlice  from "./slices/testSlice";
-
-
-const rootReducer = combineReducers({
-    counter:counterSlice
-})
-
-export default rootReducer
